3d77d8aa395d8736658ac593b949be4d9cee9e02
MariVillarroel <mvillarroel.deheza@gmail.com>
2025-07-31 21:49:56 -0400

    Implementación completa del verificador de triángulos

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #Verificador de triangulos
+
+lado1 = float(input("INGRESE EL LADO A DEL TRIANGULO: "))
+lado2 = float(input("INGRESE EL LADO B DEL TRIANGULO: "))
+lado3 = float(input("INGRESE EL LADO C DEL TRIANGULO: "))
+
+if (lado1 == lado2 == lado3):
+    print("EL TRIANGULO ES EQUILATERO")
+
+elif (lado1 != lado2 != lado3):
+    print("EL TRIANGULO ES ESCALENO")
+
+else:
+    print("EL TRIANGULO ES ISOSCELES")
